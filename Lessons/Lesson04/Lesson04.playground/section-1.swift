e4a4847922bd4d8e8b7961df52bc14afc147d04b
Rudd T <rudd@varka.la>
2014-12-15 16:17:03 -0500

    lesson 04

---
+++ Lessons/Lesson04/Lesson04.playground/section-1.swift
@@ -0,0 +1,33 @@
+// Playground - noun: a place where people can play
+// Lesson 04
+
+// Complete these, in order, writing code under each TODO statement. Each statement calls for a function to be written, write each of them and then immediately call it after the function definition.
+
+// TODO: Write a function that prints out "Hello world!" 10 times
+
+// TODO: Write a function that prints out "Hit this line {number of iterations of the loop} times!" 20 times
+
+// TODO: Write a function that accepts a string as a parameter. Print "Hello {value of string}!"
+
+// TODO: Write a function accepts a string optional. If the string optional exists, print "Hello {value of string}!". If it doesn't, print "Hello world!"
+
+// TODO: Write a function that takes one parameter, n, and returns an integer, the nth series in the fibonacci
+// sequence. The first fibonacci number is 0, the second is 1, the third is 1, the fourth is 2, fifth is 3, sixth is 5, etc. fibonacci numbers at sequence n are the sum of the n-1 and n-2 fibonacci number.
+
+// TODO: Write a function that calls the above function in order to print the sum of the first 20 fibonacci numbers.
+
+// TODO: Write a function that takes in a number and prints out whether it is prime, composite or neither.
+
+// TODO: Write a function that prints out each of the first 20 fibonacci numbers and whether they are prime. (e.g. 0 is not prime or composite, 1 is prime, etc)
+
+// TODO: Write a function that takes in two numbers, a bill amount and an optional tip percentage (represented as a float, e.g. .2 = 20% tip). Return a tuple with the total bill amount and the tip amount (if included).
+
+// TODO: Write a function that takes in a string and returns a string that is the reverse of the input. Append two strings using the + operator.
+
+// BONUS TODO: Write a function that takes in an array of strings and a search term string. Return a boolean indicating whether the search term string exists in the array.
+
+// BONUS TODO: Write a function that accepts a string and returns a boolean indicating whether a string is a palindrome (reads the same backwards or forwards).
+
+// BONUS TODO: Write a function that takes in two strings and returns a boolean indicating whether the two strings
+
+// BONUS TODO: Write a function that accepts two parameters, a string and a function that accepts a string and returns a string. Print the result of passing the string into the function 10 times.
---
+++ Lessons/Lesson04/Lesson04InClass/Lesson04InClass/ViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class ViewController: UIViewController {
+    // Functions of use for this exercise:
+    // Translating a string to an integer: "1".toInt()
+    // Getting the text from a text field: let text = textFieldName.text
+    // Setting the text on a text label or text field: textLabelName.text = "some text"
+    
+    /*
+    TODO: Create a text field, a label and a button in the storyboard's View Controller. Drag a new outlet from the label and text field to this file, then drag the 'touch up inside' event on the button into this file. When the button is clicked, make the text of the label display 'hello {whatever the text in the text field is}. Call an instructor/TA when you're done with this task.
+    */
+    
+    /*
+    TODO: Paste the fibonacci number function that you previously created into this class. Change the text box to use a numeric keypad for input. When the button is pressed, make the label print out the nth number in the fibonacci sequence (where n is the value of the text in the text field). Call an instructor/TA when you're done with this task.
+    */
+    
+    /*
+    TODO: Drag a new text field into the view controller, and create an outlet for it here. Make both text fields use the numeric keypad. One text field represents an optional tip percentage (in integer form, e.g 20 = 20% tip), the other represents the total amount of a bill. When the button is pressed, make the label display both the total amount and tip amount (e.g. "total: $100, tip: $15"). Call an instructor/TA when you're done with this task.
+    */
+}
