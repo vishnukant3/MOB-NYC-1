62211af32a844db4c8d562c5f321bb9eee360670
Rudd T <rudd@varka.la>
2015-01-28 18:27:46 -0500

    lesson 8.5

---
+++ Lessons/Lesson08.5/MyPlayground.playground/section-1.swift
@@ -0,0 +1,37 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// Arrays
+var foo: [String] = []
+var zing: Array<String> = []
+var bar = ["boooooooooo!"]
+
+// Dicts
+var baz: [String: String] = [:]
+var bif = ["foo": "bar"]
+var ping: Dictionary<String, String> = [:]
+
+foo.append("hey!")
+
+var zoom: [[String: String]] = []
+var zoom2: [Dictionary<String, String>] = []
+
+let todos = [
+    [
+        "name": "groceries",
+        "status": "done",
+        "date": "today"
+    ],
+    [
+        "name": "laundry",
+        "status": "todo",
+        "date": "tomorrow"
+    ]
+]
+
+todos[0]
+todos[1]
+
+todos[0]["name"]
+
---
+++ Lessons/Lesson08.5/Todo/Todo/ModalViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Rudd Taylor on 1/21/15.
+//  Copyright (c) 2015 GA. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    var todoViewController: MainTableViewController?
+    
+    @IBAction func didTapButton(sender: AnyObject) {
+        todoViewController?.todos.append(textField.text)
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
