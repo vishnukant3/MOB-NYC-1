361543dc17f8b2a55150152a7802e3f32000f8dd
Rudd T <rudd@varka.la>
2015-01-28 19:08:36 -0500

    array of dictionaries example

--- Lessons/Lesson08.5/MyPlayground.playground/section-1.swift
+++ Lessons/Lesson08.5/MyPlayground.playground/section-1.swift
@@ -2,6 +2,53 @@
 
 import UIKit
 
+var mysteryType: String = ""
+var anotherMystery: [String] = []
+var anotherWayToDefine = Array<String>()
+var notSoMuchAMystery = ["foo", "bar"]
+anotherMystery.append("woohoo!")
+anotherMystery[0]
+
+
+var yetAnother: [String: String] = [:]
+var anotherWayToDefineDict = Dictionary<String, String>()
+yetAnother["name"] = "rudd"
+yetAnother["name"]
+yetAnother["age"]
+
+
+var veryConfusing: [[String: String]] = []
+veryConfusing.append(["hi": "angel!"])
+
+var arrayOfDicts: [[String: String]] = [
+    [
+        "name": "laundry",
+        "date": "today",
+        "status": "done"
+    ],
+    [
+        "name": "clean house",
+        "date": "tomorrow",
+        "status": "not done"
+    ]
+]
+
+arrayOfDicts[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Arrays
 var foo: [String] = []
 var zing: Array<String> = []
