0fde1737ce61136ca59e7e2d073dac4c19552b79
Rudd T <rudd@varka.la>
2015-02-18 17:05:20 -0500

    lesson 13

---
+++ Assesments/07/Lesson07/Lesson07/ThirdViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdViewController.swift
+//  Lesson07
+//
+//  Created by Rudd Taylor on 9/30/14.
+//  Copyright (c) 2014 General Assembly. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
