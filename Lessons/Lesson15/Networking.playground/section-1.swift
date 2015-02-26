3539bdcf8eac4b5931bc09c2f0d7b61c0f06e58e
Rudd T <rudd@varka.la>
2015-02-25 19:47:41 -0500

    google printer

--- Lessons/Lesson15/Networking.playground/section-1.swift
+++ Lessons/Lesson15/Networking.playground/section-1.swift
@@ -10,18 +10,19 @@ XCPSetExecutionShouldContinueIndefinitely()
 if let url = NSURL(string: "http://google.com") {
     let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
         println(response)
+        println(data)
         var string = NSString(data: data, encoding: NSUTF8StringEncoding)
         println(string)
     })
     task.resume()
 }
 
-if let url = NSURL(string: "http://www.reddit.com/.json") {
-    let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
-        var string = NSString(data: data, encoding: NSUTF8StringEncoding)
-            println(string)
-        var json = NSJSONSerialization.JSONObjectWithData(data, options: .allZeros, error: nil)
-        println(json)
-    })
-    task.resume()
-}
+//if let url = NSURL(string: "http://www.reddit.com/.json") {
+//    let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+//        var string = NSString(data: data, encoding: NSUTF8StringEncoding)
+//            println(string)
+//        var json = NSJSONSerialization.JSONObjectWithData(data, options: .allZeros, error: nil)
+//        println(json)
+//    })
+//    task.resume()
+//}
